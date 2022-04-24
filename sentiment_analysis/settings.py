c659a6e2ded5b506cdea8a0d65a64c19044bc858
Vadim <pog120vad@gmail.com>
2022-04-24 21:52:38 +0300

    feat: added structure

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sentiment_analysis.apps.analysisapp',
+    'sentiment_analysis.apps.authapp',
 ]
 
 MIDDLEWARE = [
