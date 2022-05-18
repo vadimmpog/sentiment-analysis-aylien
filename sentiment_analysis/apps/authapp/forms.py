fc809b4a3d35b353cbb4390e0b60689196839720
Vadim Pogodin <pog120vad@gmail.com>
2022-05-18 19:40:06 +0300

    feat: authapp finished

--- sentiment_analysis/apps/authapp/forms.py
+++ sentiment_analysis/apps/authapp/forms.py
@@ -3,35 +3,6 @@ from django.contrib.auth.models import User
 from django import forms
 
 
-# class ShopUserLoginForm(AuthenticationForm):
-#
-#     def get_invalid_login_error(self):
-#         return forms.ValidationError(
-#             self.error_messages['invalid_login'],
-#             code='invalid_login',
-#             params={'username': 'телефон'},
-#         )
-#
-#     class Meta:
-#         model = ShopUser
-#         fields = ('phone', 'password')
-#
-#     def __init__(self, *args, **kwargs):
-#         super(ShopUserLoginForm, self).__init__(*args, **kwargs)
-#         for fieldname in ['username']:
-#             self.fields[fieldname].label = 'Телефон (+7)'
-#         for field_name, field in self.fields.items():
-#
-#             if field_name == 'username':
-#                 field.widget.attrs['maxlength'] = 10
-#                 field.widget.attrs['minlength'] = 10
-#
-#             field.widget.attrs['class'] = 'form-control'
-#
-#             if field_name == 'password':
-#                 field.widget.attrs['minlength'] = 8
-
-
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
